74a426692161549c37710ab983b04c08c6b61a18
Jason Ivey <jasonivey@gmail.com>
2020-12-25 15:49:21 -0700

    Add: functionality to the include paths script

--- get_include_paths.py
+++ get_include_paths.py
@@ -46,19 +46,22 @@ def _parse_args():
     parser.add_argument('-v', '--verbose', action='count', default=0, help='output verbose debugging information')
     parser.add_argument('-s', '--single-line', default=False, action='store_true',
                         help='output include paths as a single line')
+    parser.add_argument('-f', '--find-file', help='using the include paths find the file')
+    parser.add_argument('-g', '--grep', help='using the include paths grep for string')
     args = parser.parse_args()
     global _VERBOSE_LEVEL
     _VERBOSE_LEVEL = args.verbose
     verbose_print(Verbosity.INFO, f'compiler: {args.compiler}, standard library: {args.stdlib}, '
-                                  f'verbose: {args.verbose}, single line: {args.single_line}')
-    return args.compiler, args.stdlib, args.single_line
+                                  f'verbose: {args.verbose}, single line: {args.single_line}, '
+                                  f'find file: {args.find_file}, grep: {args.grep}')
+    return args.compiler, args.stdlib, args.single_line, args.find_file, args.grep
 
 
-def _run_external_shell_command(cmd):
+def _run_external_shell_command(cmd, ignore_error=False):
     process = subprocess.Popen(cmd, shell=True, encoding='utf-8', bufsize=1,
                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdoutdata, stderrdata = process.communicate()
-    if process.wait() != 0:
+    if process.wait() != 0 and not ignore_error:
         verbose_print(Verbosity.ERROR, f'"{cmd}" returned error code: {process.returncode}')
         return None
     verbose_print(Verbosity.INFO, f'INFO: "{cmd}" returned: {stdoutdata}')
@@ -93,8 +96,22 @@ def find_system_include_paths(compiler, stdlib=None):
                 yield path.resolve()
 
 
+def find_file_in_path(include_path, file_to_find):
+    for path in include_path.rglob(file_to_find):
+        yield path.resolve()
+
+
+def find_str_in_path(include_path, search_str):
+    command = f'find -H {include_path} -type f -exec grep "{search_str}" {{}} \+'
+    output = _run_external_shell_command(command, ignore_error=True)
+    if not output:
+        return
+    for line in output.splitlines():
+        yield line.strip()
+
+
 def main():
-    compiler, stdlib, single_line = _parse_args()
+    compiler, stdlib, single_line, file_to_find, grep_str = _parse_args()
     try:
         single_line_str = ''
         for path in find_system_include_paths(compiler, stdlib):
@@ -102,6 +119,12 @@ def main():
                 single_line_str += f'{path}' if len(single_line_str) == 0 else f'{os.path.pathsep}{path}'
             else:
                 print(path)
+                if file_to_find:
+                    for found_path in find_file_in_path(path, file_to_find):
+                        print(f'  [FILE FOUND]: {found_path}')
+                if grep_str:
+                    for found_path in find_str_in_path(path, grep_str):
+                        print(f'  [{grep_str} FOUND]: {found_path}')
         if single_line:
             print(single_line_str)
     except:

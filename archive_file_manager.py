075c2a62234d665a013158cd2da3629aa8d106d7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 09:29:41 +0000

    Adding tarfile member sanitization to extractall()

--- archive_file_manager.py
+++ archive_file_manager.py
@@ -187,7 +187,26 @@ def list_tar(args):
 
 def extract_tar(args):
     with tarfile.open(args.filename, 'r:*') as tar:
-        tar.extractall(path=args.destination, numeric_owner=True)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=args.destination, numeric_owner="True")
 
 def _parse_args():
     parser = argparse.ArgumentParser(description='Create, list and extract tape archive (tar) files')
